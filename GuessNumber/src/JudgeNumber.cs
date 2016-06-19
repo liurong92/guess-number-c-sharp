f0468d2664633be0093056d53e2a0d213612eeb6
liurong92 <liurong92@outlook.com>
2016-06-19 13:50:17 +0800

    init project and can guess 4a0b and 0a0b.

---
+++ GuessNumber/src/JudgeNumber.cs
@@ -0,0 +1,18 @@
+﻿namespace GuessNumber
+{
+    public class JudgeNumber
+    {
+        public string GetScore(int[] systemNumber, int[] inputNumber)
+        {
+            int resultA = 0;
+            for (int i = 0; i < inputNumber.Length; i++)
+            {
+                if (inputNumber[i] == systemNumber[i])
+                {
+                    resultA++;
+                }
+            }
+            return resultA.ToString() + "A0B";
+        }
+    }
+}
---
+++ GuessNumber/test/GuessNumber.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GuessNumber.test
+{
+    [TestClass]
+    public class GuessNumber
+    {
+        [TestMethod]
+        public void should_return_4A0B_when_system_number_is_1234_and_input_number_is_1234()
+        {
+            var JudgeNumber = new JudgeNumber();
+            Assert.AreEqual("4A0B", JudgeNumber.GetScore(new[] {1,2,3,4}, new[] {1,2,3,4}));
+        }
+
+        [TestMethod]
+        public void should_return_0A0B_when_system_number_is_1234_and_input_number_is_5678()
+        {
+            var JudgeNumber = new JudgeNumber();
+            Assert.AreEqual("0A0B", JudgeNumber.GetScore(new[] {1,2,3,4}, new[] {5,6,7,8}));
+        }
+    }
+}
