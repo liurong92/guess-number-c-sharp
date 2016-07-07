003bdd99aafb0426add29c5057ec6c4da5abb78a
liurong <liurong92@outlook.com>
2016-07-08 00:01:11 +0800

    add generator random function

---
+++ GuessNumber/src/Generator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GuessNumber
+{
+    public class Generator
+    {
+        public string getRandomNumber()
+        {
+            string systemValue = string.Empty;
+            while (systemValue.Length < 4)
+            {
+                var random = new Random();
+                int randomNumber = random.Next(0, 10);
+                if (!systemValue.Contains(randomNumber.ToString()))
+                {
+                    systemValue += randomNumber.ToString();
+                }
+            }
+            return systemValue;
+        }
+    }
+}
---
+++ GuessNumber/test/GeneratorTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GuessNumber.test
+{
+    [TestClass]
+    public class GeneratorTest
+    {   
+        [TestMethod]
+        public void should_return_the_value_length_is_4()
+        {
+            var Generator = new Generator();
+            var result = Generator.getRandomNumber();
+            Assert.AreEqual(4, result.ToCharArray().Length);
+        }
+
+        [TestMethod]
+        public void should_return_the_value_is_number()
+        {
+            var Generator = new Generator();
+            var result = Generator.getRandomNumber();
+            var resultArray = result.ToCharArray();
+
+            int number = 0;
+            for (var i = 0; i < resultArray.Length; i++)
+            {
+                for (var j = 0; j < resultArray.Length; j++)
+                {
+                    if (resultArray[i] == resultArray[j] && i != j)
+                    {
+                        number++;
+                    }
+                }
+            }
+
+            Assert.AreEqual(0, number);
+
+        }
+    }
+}
